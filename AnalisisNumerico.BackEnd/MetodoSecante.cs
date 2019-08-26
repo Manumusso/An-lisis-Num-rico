8b3282b974837640ccb4f63bb91aa46ba5464658
MatiasDBertone <matibertone@live.com.ar>
2019-08-26 20:27:35 -0300

    no message

---
+++ AnalisisNumerico.BackEnd/MetodoSecante.cs
@@ -0,0 +1,14 @@
+﻿using AnalisisNumerico.BackEnd.Logica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalisisNumerico.BackEnd
+{
+    class MetodoSecante: MetodosRaices
+    {
+
+    }
+}
